605d10fc225710aa131942613869f198aa1043cd
Jari Salojuuri <jari.salojuuri@gmail.com>
2020-07-23 10:23:38 +0300

    lisätty front build

---
+++ build/precache-manifest.b0ab184f0401660e6830659a86a36317.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a0206b02ccbf0e96d756e1775756a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d219f0a19fa3df783e20",
+    "url": "/static/css/main.c62829e1.chunk.css"
+  },
+  {
+    "revision": "d72cc7c3f59543947486",
+    "url": "/static/js/2.029bf66b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.029bf66b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d219f0a19fa3df783e20",
+    "url": "/static/js/main.e9b98c3a.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
